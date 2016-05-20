499aec83406e73d0fc9284134bcd4d2eea654849
Janelle Nomad <jheron3@my.centennialcollege.ca>
2016-05-20 09:55:46 -0400

    The purpose of this program is to practice for quiz 1, and is should  not be graded for marks, as this is practice only
    Version 0.1 - Intial version

---
+++ PracticeExercise_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * Author: Janelle Heron
+ * Student ID: 300839820
+ * 
+ * Description: The purpose of this program is to practice for quiz 1, and is should  
+ * not be graded for marks, as this is practice only =)
+ * 
+ * Version 0.1 - Intial Version
+ */
+namespace PracticeExercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
